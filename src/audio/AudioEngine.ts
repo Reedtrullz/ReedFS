9183f36fda725902ce3db91882b8b17269b1e1a6
Reidar <reidjoss@gmail.com>
2026-05-25 09:17:07 +0200

    feat: add Web Audio engine foundation

---
+++ src/audio/AudioEngine.ts
@@ -0,0 +1,48 @@
+export class AudioEngine {
+  ctx: AudioContext;
+  master: GainNode;
+  engineBus: GainNode;
+  cockpitBus: GainNode;
+  private _started = false;
+
+  constructor() {
+    this.ctx = new AudioContext();
+    this.master = this.ctx.createGain();
+    this.master.gain.value = 0.5;
+    this.master.connect(this.ctx.destination);
+
+    this.engineBus = this.ctx.createGain();
+    this.engineBus.gain.value = 0.8;
+    this.engineBus.connect(this.master);
+
+    this.cockpitBus = this.ctx.createGain();
+    this.cockpitBus.gain.value = 0.6;
+    this.cockpitBus.connect(this.master);
+
+    this._started = false;
+  }
+
+  async start() {
+    if (this.ctx.state === 'suspended') {
+      await this.ctx.resume();
+    }
+    this._started = true;
+  }
+
+  get started() { return this._started; }
+
+  setMasterVolume(v: number) {
+    this.master.gain.value = Math.max(0, Math.min(1, v));
+  }
+
+  dispose() {
+    this.ctx.close();
+  }
+}
+
+let instance: AudioEngine | null = null;
+
+export function getAudioEngine(): AudioEngine {
+  if (!instance) instance = new AudioEngine();
+  return instance;
+}
